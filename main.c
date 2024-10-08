648fb9d5fe539e1089dca46e081af16bd057ef8c
Pedro Leite Pereira <164282204+pedroleitep@users.noreply.github.com>
2024-10-08 12:23:32 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Faça um programa que leia cinco grupos de quatro valores (A, B, C, D) e mostre-os na ordem lida. Em segui
+da, organize-os em ordem crescente e decrescente.
+*/
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
